ebdbfc40c5f4d3faf326b494bc45749d992996dd
felipe <angelvelandia1815@gmail.com>
2022-07-25 16:29:33 -0400

    adding cards and search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,22 +2,29 @@
 var weightEl = document.querySelector("#lbs");
 var dosageEl = document.querySelector("#dosge");
 
+
+
+
 // event listener for wight
 weightEl.addEventListener('click', function () {
 
-    // fetch 
-    fetch(`https://api.fda.gov/drug/drugsfda.json?search=${weightEl.value}products.dosage_form:"LOTION"&limit=1`)
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (data) {
-            console.log(data)
-        })
+    // // fetch 
+    // fetch(`https://api.fda.gov/drug/drugsfda.json?search=${weightEl.value}products.dosage_form:"LOTION"&limit=1`)
+    //     .then(function (response) {
+    //         return response.json()
+    //     })
+    //     .then(function (data) {
+    //         console.log(data)
+    //     })
     // save users value
     var input = document.getElementById("input").value
     console.log(input)
 
     // display in the output 
 
-    document.getElementById('output').value = input / 2.205 + "kg"
+    // document.getElementById('output').value = input / 2.2 + "kilograms"
+
+    var output = document.getElementById('output').value = (input / 2.2).toFixed(2)
+    console.log(lbs)
+
 })
